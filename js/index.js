a3ac1d7fbb6189e6079049012c9f76bf5cfa4ef5
Nolo-98 <twisterfast@gmail.com>
2021-06-11 20:08:11 -0500

    avance

---
+++ js/index.js
@@ -0,0 +1,20 @@
+//declaracion de variables
+const usuario = document.getElementById('user');
+const password = document.getElementById('pass');
+const formulario = document.getElementById('formLogin');
+
+formulario.addEventListener('submit', login);
+
+//funciones a ejecutar
+function login(e){
+    e.preventDefault();
+
+    let usuarioVal = usuario.value;
+    let passwordVal = password.value;
+
+    if(usuarioVal == '' || passwordVal == '') {
+        return;
+    }
+    console.log('el valor para user es:' + usuarioVal);
+    console.log('el valor para pass es:' + passwordVal)
+}
---
+++ js/registro.js
@@ -0,0 +1,49 @@
+// Declaracion de variables de referencia de objetos
+const nombre = document.getElementById('name');
+const email = document.getElementById('email');
+const user = document.getElementById('user');
+const pass = document.getElementById('pass');
+const formulario = document.getElementById('formRegistro');
+
+formulario.addEventListener('submit', registro);
+
+function registro(e){
+    e.preventDefault();
+
+    let nombreVal = nombre.value;
+    let emailVal = email.value;
+    let userVal = user.value;
+    let passValue = pass.value;
+
+    if(nombreVal == '' || emailVal == '' || userVal == '' || passValue == ''){
+        creaMensaje('Verifica tus campos', 'danger');
+        return;
+    }
+
+    const usuario = {
+        nombre: nombreVal,
+        email: emailVal,
+        user: userVal,
+        pass: passValue
+    }
+    
+    localStorage.getItem('usuario', JSON.stringify(usuario));
+
+    nombre.value = '';
+    email.value = '';
+    user.value = '';
+    pass.value = '';
+
+    creaMensaje('Usuario Registrado!', 'success');
+}
+
+function creaMensaje(texto, tipo){
+    const nuevoElemento = document.createElement('div');
+    nuevoElemento.innerText = texto;
+    nuevoElemento.classList.add('alert', 'alert-' + tipo);
+    const divMensaje = document.getElementById('mensaje');
+    divMensaje.appendChild(nuevoElemento);
+    setTimeout(function(){
+        nuevoElemento.remove();
+    }, 2000);
+}
